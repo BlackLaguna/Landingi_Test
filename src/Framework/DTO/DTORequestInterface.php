a5a43875c116b46514900d4b576d59d2a01f20aa
Kruchenov Andrii <kruchenow@gmail.com>
2021-10-30 22:41:46 +0200

    remove constructor in contract

--- src/Framework/DTO/DTORequestInterface.php
+++ src/Framework/DTO/DTORequestInterface.php
@@ -3,9 +3,7 @@ declare(strict_types=1);
 
 namespace RecruitmentApp\Framework\DTO;
 
-use Symfony\Component\HttpFoundation\Request;
-
 interface DTORequestInterface
 {
-    public function __construct(Request $request);
+
 }
